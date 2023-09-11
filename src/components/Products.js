b7b5fbb3dca050f4678387bb827ec11b6e409adf
dally399 <darrenkibande@gmail.com>
2023-09-11 10:57:03 +0300

    monday update

--- src/components/Products.js
+++ src/components/Products.js
@@ -9,6 +9,10 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 //other imports
 import product1 from '../assets/product-fuerte.jpeg'
+import hass from '../assets/product-hass.jpeg'
+import pineapples from '../assets/pinapple.jpg'
+import mango from '../assets/Mango'
+import fuerte from '../assets/product-fuerte.jpeg'
 
 function Products() {
   return (
@@ -17,7 +21,7 @@ function Products() {
       <span className='description-heading'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam delectus sint illum labore sit error placeat nostrum nemo? Laboriosam, iure.</span>
     <div className="body-container">
       <div className="content-left">
-     
+          <img src={hass} alt="" />
       </div>
       <div className="content-right">
         <span className='hass-title'>Hass Avocados</span>
@@ -30,7 +34,24 @@ function Products() {
       </div>
 
       <div className="content-right-mid">
+        <img src={fuerte} alt="" />
+      </div>
 
+      <div className="content-left">
+        <img src={pineapples} alt="" />
+      </div>
+      <div className="content-right">
+        <span className='hass-title'>Pineapples</span>
+        <span className='hass-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eveniet repellat molestias suscipit culpa hic maxime amet non excepturi saepe officia rerum sequi, earum explicabo laboriosam assumenda dolorem labore eum.</span>
+      </div>
+ 
+      <div className="content-left-mid">
+        <span className='fuerte-title'>Mangos</span>
+        <span className='fuerte-container'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nihil iusto odit facere, perferendis cum id? Nihil ducimus excepturi enim molestias eligendi? Porro, assumenda! Atque suscipit debitis non eos modi.</span>
+      </div>
+
+      <div className="content-right-mid">
+          <img src={mango} alt="" />
       </div>
     </div>
 
