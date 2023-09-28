b7f866e8f00c7154a8ba3ea6fce91e9c94c2fec2
dally399 <darrenkibande@gmail.com>
2023-09-28 08:24:44 +0300

    thursday

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ import 'swiper/css/navigation';
 import image1 from "../assets/hero.jpg"
 import image2 from "../assets/hero-pineapple.jpg"
 import image3 from "../assets/alexander-schimmeck-vTXtQ8ZBzvY-unsplash.jpg"
-import main from "../assets/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+
 
 import './Hero.css'
 import 'animate.css';
@@ -38,23 +38,7 @@ export default function Hero() {
         modules={[EffectFade,Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide style={{height:"100%"}}> 
-        
-    <div className="background-container">
-        <img src={main} alt="" loading='lazy'/>
-      <div className="left-side">
-        
-        <span className='h-top animate__animated animate__bounceInDown'>A New Way To Invest In Agriculture </span> <br />
-        <span className='h-mid animate__animated animate__bounceInDown'>Experience The Real</span> <br />
-        <span className='h-below animate__animated animate__bounceInDown'>Agriculture</span>
-
-       <Link className="link" to='/about'> <button className='Btn-hero animate__animated animate__bounceInDown'  >Read More</button> </Link> 
-         
-      </div>     
-    </div>
-
-
-         </SwiperSlide>
+       
         <SwiperSlide style={{height:"100%"}}>
         <div className="background-container">
         <img src={image1} alt="" loading='lazy'/>
@@ -63,7 +47,7 @@ export default function Hero() {
         <span className='h-mid animate__animated animate__bounceInDown'>AVOCADOS</span> <br />
         <span className='h-below animate__animated animate__bounceInDown'>By HortFarms</span>
 
-       <Link className="link" to='/products'> <button className='Btn-hero animate__animated animate__bounceInDown'  >Products</button> </Link> 
+       <Link className="link" to='/products'> <button className='Btn-hero animate__animated animate__bounceInDown'  >See Products</button> </Link> 
          
       </div>     
     </div>
